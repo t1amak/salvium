ac63346fbfc6b16166c9bf10ccab4bed99c3792c
gh14 <57602242+t1amak@users.noreply.github.com>
2025-10-31 09:33:33 +0100

    Update FreeBSD base package version to 13.4

--- contrib/depends/packages/freebsd_base.mk
+++ contrib/depends/packages/freebsd_base.mk
@@ -1,9 +1,9 @@
 package=freebsd_base
-$(package)_version=13.3
+$(package)_version=13.4
 $(package)_download_path=https://download.freebsd.org/ftp/releases/amd64/$($(package)_version)-RELEASE/
 $(package)_download_file=base.txz
 $(package)_file_name=freebsd-base-$($(package)_version).txz
-$(package)_sha256_hash=815e21952d19456209b55d78a1a36720d5755106a7f71b9c24e6c9c0b11153d8
+$(package)_sha256_hash=8e13b0a93daba349b8d28ad246d7beb327659b2ef4fe44d89f447392daec5a7c
 
 define $(package)_extract_cmds
   echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
