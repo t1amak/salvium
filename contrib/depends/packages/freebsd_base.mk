e36e7311ebd9b4192fd425070571d727b0f2fd3a
gh14 <57602242+t1amak@users.noreply.github.com>
2025-10-31 09:45:15 +0100

    Refactor commands in freebsd_base.mk for consistency

--- contrib/depends/packages/freebsd_base.mk
+++ contrib/depends/packages/freebsd_base.mk
@@ -4,20 +4,17 @@ $(package)_download_path=https://download.freebsd.org/ftp/releases/amd64/$($(pac
 $(package)_download_file=base.txz
 $(package)_file_name=freebsd-base-$($(package)_version).txz
 $(package)_sha256_hash=8e13b0a93daba349b8d28ad246d7beb327659b2ef4fe44d89f447392daec5a7c
-
 define $(package)_extract_cmds
-  echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
-  tar xf $($(1)_source_dir)/$($(package)_file_name) ./lib/ ./usr/lib/ ./usr/include/
+	echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
+	tar xf $($(1)_source_dir)/$($(package)_file_name) ./lib/ ./usr/lib/ ./usr/include/
 endef
-
 define $(package)_build_cmds
-  mkdir bin &&\
-  echo "#!/bin/sh\n\nexec /usr/bin/clang-14 -target x86_64-unknown-freebsd$($(package)_version) --sysroot=$(host_prefix)/native $$$$""@" > bin/clang-14 &&\
-  echo "#!/bin/sh\n\nexec /usr/bin/clang++-14 -target x86_64-unknown-freebsd$($(package)_version) --sysroot=$(host_prefix)/native $$$$""@" > bin/clang++-14 &&\
-  chmod 755 bin/*
+	mkdir bin &&\
+	echo "#!/bin/sh\n\nexec /usr/bin/clang-14 -target x86_64-unknown-freebsd$($(package)_version) --sysroot=$(host_prefix)/native $$$$""@" > bin/clang-14 &&\
+	echo "#!/bin/sh\n\nexec /usr/bin/clang++-14 -target x86_64-unknown-freebsd$($(package)_version) --sysroot=$(host_prefix)/native $$$$""@" > bin/clang++-14 &&\
+	chmod 755 bin/*
 endef
-
 define $(package)_stage_cmds
-  mkdir $($(package)_staging_dir)/$(host_prefix)/native &&\
-  mv bin lib usr $($(package)_staging_dir)/$(host_prefix)/native
+	mkdir $($(package)_staging_dir)/$(host_prefix)/native &&\
+	mv bin lib usr $($(package)_staging_dir)/$(host_prefix)/native
 endef
